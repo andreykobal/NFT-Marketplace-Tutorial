558640e60cae9b988bb10c13d03c68728e877a10
Andrew Kobal <kobalandrey@gmail.com>
2023-10-31 20:24:23 +0000

    fix bigint issue

--- scripts/execute_scenario.js
+++ scripts/execute_scenario.js
@@ -26,13 +26,13 @@ async function main() {
         const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
 
         // Send 0.015 ETH from Wallet 1 to the new wallet
-        console.log(`Creating Wallet ${i} and sending 0.015 ETH to it...`);
+        console.log(`Creating Wallet ${i} and sending 0.003 ETH to it...`);
         const tx2 = await wallet1.sendTransaction({
             to: wallet.address,
-            value: ethers.utils.parseEther("0.015"),
+            value: ethers.utils.parseEther("0.003"),
         });
         await tx2.wait();
-        console.log(`0.015 ETH sent to Wallet ${i} at address: ${wallet.address}`);
+        console.log(`0.003 ETH sent to Wallet ${i} at address: ${wallet.address}`);
 
         // Buy the token from Wallet 2
         const marketplaceContractWithWallet = await ethers.getContractAt("NFTMarketplace", marketplaceAddress, wallet);
@@ -41,8 +41,12 @@ async function main() {
         await tx3.wait();
         console.log(`Wallet ${i} has bought the NFT with tokenId: ${tokenId.toString()}`);
 
-        // Update the token price by 10%
-        price = ethers.utils.parseEther("0.0012");
+        // Update the token price by a random percentage within the range of -10% to +10%
+        const originalPrice = ethers.utils.formatUnits(price, 18); // Convert to a decimal number
+        const randomPercentage = Math.random() * 0.2 - 0.1; // Random value between -0.1 and +0.1
+        const newPrice = originalPrice * (1 + randomPercentage);
+        // Round the new price to 6 decimal places
+        price = ethers.utils.parseEther(newPrice.toFixed(6));
         console.log(`Wallet ${i} is updating the token price...`);
         const tx4 = await marketplaceContractWithWallet.updateTokenPrice(tokenId, price);
         await tx4.wait();
